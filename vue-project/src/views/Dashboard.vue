5c67277e4aad90d8457439711cc45bab039453b0
David <davidjmoed@gmail.com>
2020-12-01 15:24:41 +0200

    Add logout functionality to dashboard

--- vue-project/src/views/Dashboard.vue
+++ vue-project/src/views/Dashboard.vue
@@ -5,5 +5,27 @@
         <router-link to="/dashboard">Dashboard</router-link>
      </div>
      <h1>This is a dashboard</h1>
+     <h3>Data would go here in some fun styled widgets!</h3>
   </div>
 </template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+   name: 'Home',
+   computed: mapGetters(['user']),
+   methods: {
+      ...mapActions(['LogOut']),
+      async logout() {
+         try {
+            await this.LogOut();
+            this.$router.push('/login');
+            debugger;
+         } catch (err) {
+            console.log(err)
+         }
+      }
+   }
+}
+</script>
